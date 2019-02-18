fbeeab585cc5dd11ad16b8806b93f944b4635a7a
James Sutherland <jrsutherland78@googlemail.com>
2019-02-18 14:30:04 +0000

    first commit, setup folders, rakefile, rspec

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+ruby '2.5.3'
+
+gem 'bcrypt'
+gem 'dotenv'
+gem 'pg'
+gem 'rake'
+gem 'sinatra'
+gem 'sinatra-flash'
+
+group :test do
+  gem 'capybara'
+  gem 'rspec'
+  gem 'rubocop'
+  gem 'simplecov'
+  gem 'simplecov-console'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+ENV['RACK_ENV'] = 'test'
+
+require './app'
+require 'capybara/rspec'
+require 'simplecov'
+require 'simplecov-console'
+require 'test_database_setup'
+
+Capybara.app = UsersAPI
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+])
+SimpleCov.start
+
+RSpec.configure do |config|
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+end
