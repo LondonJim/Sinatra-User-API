61a965695e8b2e5bb33c962c7166ff119ae11fef
James Sutherland <jrsutherland78@googlemail.com>
2019-02-18 20:33:14 +0000

    add bcrypt password to user signup, need user details to delete

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'bcrypt'
 require 'json'
 require 'sinatra/base'
 require 'sinatra/activerecord'
@@ -5,18 +6,33 @@ require './models/user'
 
 class UsersAPI < Sinatra::Base
 
+  include BCrypt
+
   post '/users/signup' do
     data = JSON.parse(request.body.read)
-    User.create(:name => data["name"], :email => data["email"], :password => data["password"])
+    encrypted_pw = Password.create(data["password"])
+    User.create(:name => data["name"], :email => data["email"], :password => encrypted_pw)
   end
 
   post '/users/login' do
     data = JSON.parse(request.body.read)
-    User.exists?(:name => data["name"], :password => data["password"])
+    user = User.find_by(:name => data["name"], :email => data["email"])
+    if Password.new(user[:password]) == data["password"]
+      puts "VALID"
+    else
+      puts "NOT VALID"
+    end
   end
 
   delete '/users/delete/:id' do
-    User.destroy(params[:id])
+    data = JSON.parse(request.body.read)
+    user = User.find_by(:name => data["name"], :email => data["email"])
+    if Password.new(user[:password]) == data["password"] && user.id = params[:id]
+      puts "DELETED"
+      User.destroy(params[:id])
+    else
+      puts "USER DETAILS NOT CORRECT"
+    end
   end
 
 end
